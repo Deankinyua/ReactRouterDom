e430ab47c78525f8509ffaa458e1218ead2ad476
deankinyua <kinyuadean@gmail.com>
2024-03-06 22:47:36 +0300

    First commit of React Router Dom

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+import {
+  createBrowserRouter,
+  Route,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+
+// pages
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Faq from "./pages/Help/Faq";
+import Contact from "./pages/Help/Contact";
+
+// Layouts
+import RootLayout from "./layouts/RootLayout";
+import HelpLayout from "./layouts/HelpLayout";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+      <Route index element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="help" element={<HelpLayout />}>
+        <Route path="faq" element={<Faq />}></Route>
+        <Route path="contact" element={<Contact />}></Route>
+      </Route>
+    </Route>
+  )
+);
+
+function App() {
+  return <RouterProvider router={router}></RouterProvider>;
+}
+
+export default App;
