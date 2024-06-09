2e7f5df6b18ce572f941ff429b026ef497d154c0
deankinyua <kinyuadean@gmail.com>
2024-06-09 13:33:26 +0300

    reviving this project

--- src/App.tsx
+++ src/App.tsx
@@ -10,10 +10,16 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Faq from "./pages/Help/Faq";
 import Contact from "./pages/Help/Contact";
+import NotFound from "./pages/NotFound";
+import Careers from "./pages/careers/Careers";
 
 // Layouts
 import RootLayout from "./layouts/RootLayout";
 import HelpLayout from "./layouts/HelpLayout";
+
+// Loaders
+import { careerDetailsLoader, careersLoader } from "./loaders/careersLoader";
+import CareerDetails from "./pages/careers/CareerDetails";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -24,6 +30,17 @@ const router = createBrowserRouter(
         <Route path="faq" element={<Faq />}></Route>
         <Route path="contact" element={<Contact />}></Route>
       </Route>
+
+      <Route path="careers">
+        <Route index element={<Careers />} loader={careersLoader}></Route>
+        <Route
+          path=":id"
+          element={<CareerDetails />}
+          loader={careerDetailsLoader}
+        ></Route>
+      </Route>
+
+      <Route path="*" element={<NotFound />}></Route>
     </Route>
   )
 );
---
+++ src/loaders/careersLoader.ts
@@ -0,0 +1,10 @@
+export const careersLoader = async () => {
+  const res = await fetch("http://localhost:4000/careers");
+  return res.json();
+};
+
+export const careerDetailsLoader = async ({ params }) => {
+  const { id } = params;
+  const res = await fetch("http://localhost:4000/careers/" + id);
+  return res.json();
+};
